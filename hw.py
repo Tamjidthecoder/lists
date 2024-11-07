d964882b4e2f70d591556fbc471f18b4bd56c563
Tamjidthecoder <tamjidcool797@gmail.com>
2024-11-07 23:23:53 +0600

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,8 @@
+# Create a list of numbers from 1 to 10
+numbers = list(range(1, 11))
+
+# Use list comprehension to create a list of their squares
+squares = [number ** 2 for number in numbers]
+
+# Print the list of squares
+print(squares)
